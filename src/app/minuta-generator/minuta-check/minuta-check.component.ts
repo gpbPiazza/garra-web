aaea921a7354352542023aa231ca4ad2a157aea7
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2025-05-20 08:40:21 -0300

    feat: implement edit mode hightlight the missing tokens

--- src/app/minuta-generator/minuta-check/minuta-check.component.ts
+++ src/app/minuta-generator/minuta-check/minuta-check.component.ts
@@ -32,30 +32,37 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
           <div *ngIf="tokensNotFound?.length" class="tokens-warning">
             <h3>Atenção: Alguns dados não foram encontrados</h3>
             <p>Por favor, verifique e complete os seguintes campos na minuta:</p>
-            <mat-chip-set>
-              <mat-chip 
+            <p>Edite os valores não encontrados clicando aqui abaixo.</p>
+            <mat-card-actions>
+              <button 
+                mat-raised-button
+                [disabled]="!editMode"
                 *ngFor="let token of tokensNotFound" 
-                highlighted>
-                {{ token }}
-              </mat-chip>
-            </mat-chip-set>
+                highlighted
+                (click)="focusOnToken(token)">
+                  {{ token }}
+              </button>
+          </mat-card-actions>
           </div>
 
-        <div class="minuta-editor-container">
+        <div class="minuta-editor-container" [class.editing-mode]="editMode">
           <div #minutaEditor 
               class="minuta-editor" 
-              [class.highlight-missing]="tokensNotFound.length" 
-              [attr.contenteditable]=this.editMode
+              [attr.contenteditable]="editMode"
               (input)="onEditorInput()">
           </div>
         </div>
       </mat-card-content>
       
       <mat-card-actions align="end">
-        <button mat-button mat-raised-button (click)="onEditMode()">
+        <button 
+          mat-button 
+          mat-raised-button 
+          [class.attention-needed]="tokensNotFound.length > 0 &&  !editMode"
+          (click)="onEditMode()">
           <mat-icon>{{ editMode ? 'check' : 'edit' }}</mat-icon>
           {{ editMode ? 'FINALIZAR EDIÇÃO' : 'EDITAR' }}
-      </button>
+        </button>
         <button mat-button mat-raised-button [disabled]=editMode (click)="onDownload()">
           <mat-icon>download</mat-icon>
           EXPORTAR
@@ -85,14 +92,17 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
       color: var(--mat-sys-on-error-container);
     }
     
-
     .minuta-editor-container {
       border-radius: 4px;
       padding: 1rem;
       box-shadow: var(--mat-sys-level2);
-      background-color: var(--mat-sys-surface);
     }
     
+    .editing-mode {
+      border: 2px solid var(--mat-sys-primary);
+      background-color: var(--mat-sys-surface-bright)
+    }
+
     .minuta-editor {
       min-height: 300px;
       max-height: 600px;
@@ -100,6 +110,29 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
       outline: none;
       font-size: 16px;
       line-height: 1.5;
+    }
+
+    .attention-needed {
+      animation: pulse 1.5s infinite;
+      margin-right: 1rem;
+    }
+
+    @keyframes pulse {
+      0% {
+        transform: scale(1.2);
+      }
+      70% {
+        transform: scale(1.3);
+      }
+      100% {
+        transform: scale(1.2);
+      }
+    }
+
+    mat-card-actions  {
+      button {
+        margin-left: 0.5rem;
+      }
     }
   `]
 })
@@ -122,7 +155,7 @@ export class MinutaCheckComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     if (!this.minutaEditor) return;
-    if (!this.content) return ;
+    if (!this.content) return;
     
     this.minutaEditor.nativeElement.innerHTML = this.content; 
 
@@ -140,7 +173,7 @@ export class MinutaCheckComponent implements AfterViewInit {
 
       highlightedContent = highlightedContent.replace(
         tokenRegex,
-        `<span style="color: var(--mat-sys-on-error-container);">[[${token} NÃO ENCONTRADO]]</span>`
+        `<span id="${token}">[[${token} NÃO ENCONTRADO]]</span>`
       );
     });
 
@@ -148,12 +181,39 @@ export class MinutaCheckComponent implements AfterViewInit {
     this.minutaEditor.nativeElement.innerHTML = highlightedContent;
   }
 
+  focusOnToken(token: string): void {
+    if (!this.editMode) {
+      this.onEditMode()
+    }
+
+    const tokenElement = document.getElementById(`${token}`)
+    if (!tokenElement) return;
+
+    const range = document.createRange();
+    range.selectNode(tokenElement);
+    const selection = window.getSelection();
+
+    if (!selection) return;
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    const handleKeyPress = (event: KeyboardEvent) => {
+      this.tokensNotFound = this.tokensNotFound.filter(t => t !== token)
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  
+    document.addEventListener('keydown', handleKeyPress);
+
+    this.snackBar.open(`Editando: ${token}`, 'OK', {duration: 3000});
+  }
+
   onEditorInput(): void {
     this.editedContent = this.minutaEditor.nativeElement.innerHTML;
   }
 
   onEditMode(): void {
-    this.editMode = this.editMode ? false : true;
+    this.editMode = !this.editMode;
   }
 
   onCopy() {
@@ -169,8 +229,7 @@ export class MinutaCheckComponent implements AfterViewInit {
   onNewMinuta() {
     this.editedContent = '';
     this.content = '';
-    this.editedContent = '';
-    this.newMinuta.emit()
+    this.newMinuta.emit();
   }
 
   onDownload(): void {
